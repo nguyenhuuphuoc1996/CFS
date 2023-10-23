d2a8afca12652b66d8af1facc91eda986333914a
phuocnguyenhuu <phuocnguyenhuu96@gmail.com>
2023-10-23 17:34:07 +0700

    Add stubs function as a job

---
+++ ProcessJobFunction.cpp
@@ -0,0 +1,1 @@
+#include "ProcessJobFunction.h"
---
+++ ProcessJobFunction.h
@@ -0,0 +1,6 @@
+#ifndef PROCESS_JOB_FUNCTION
+#define PROCESS_JOB_FUNCTION
+#include <iostream>
+
+// This file contains implementation of functions that are mapping with every node in process task
+#endif
